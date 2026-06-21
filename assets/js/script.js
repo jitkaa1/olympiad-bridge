59a8f54e0ed82c3122d0da77b3fe99488133792b
jitkaa1 <nikkirk2011@icloud.com>
2026-06-21 19:07:17 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
+function sendMail(){
+  let parms = {
+    name : document.getElementById("name").value, 
+    email : document.getElementById("email").value, 
+    message : document.getElementById("message").value, 
+  }
 
+  emailjs.send("service_wl5a0ak","template_9exyt7s",parms).then(alert("Email Sent!"))
+}
